9c17b60ce7f68098235765eeca8010bf94f8e8d6
vborovik <w.borovik@mail.ru>
2023-08-14 14:05:22 +0300

    COMPETENCE-5 - добавлены файлы wsdl и xsd

--- service/src/main/java/ru/axbit/service/service/CustomerService.java
+++ service/src/main/java/ru/axbit/service/service/CustomerService.java
@@ -1,4 +1,8 @@
 package ru.axbit.service.service;
 
+import ru.axbit.vborovik.competence.core.CustomerPageType;
+import ru.axbit.vborovik.competence.filtertypes.GetCustomerListFilterType;
+
 public interface CustomerService {
+    CustomerPageType getCustomer(GetCustomerListFilterType body);
 }
--- service/src/main/java/ru/axbit/service/service/impl/CustomerServiceImpl.java
+++ service/src/main/java/ru/axbit/service/service/impl/CustomerServiceImpl.java
@@ -2,7 +2,17 @@ package ru.axbit.service.service.impl;
 
 import org.springframework.stereotype.Service;
 import ru.axbit.service.service.CustomerService;
+import ru.axbit.vborovik.competence.core.CustomerPageType;
+import ru.axbit.vborovik.competence.filtertypes.GetCustomerListFilterType;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public CustomerPageType getCustomer(GetCustomerListFilterType body) {
+        var response = new CustomerPageType();
+        var id = body.getCustomerId();
+
+        return response;
+    }
 }
