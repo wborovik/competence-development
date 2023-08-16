618d44f1f67cedaa4d4fdd0bb855ddf7c6d88b91
vborovik <w.borovik@mail.ru>
2023-08-16 11:04:09 +0300

    COMPETENCE-9 - добавлен метод getExecutorList

--- domain/src/main/java/ru/axbit/domain/repository/ExecutorRepository.java
+++ domain/src/main/java/ru/axbit/domain/repository/ExecutorRepository.java
@@ -3,5 +3,8 @@ package ru.axbit.domain.repository;
 import ru.axbit.domain.domain.user.Executor;
 import ru.axbit.domain.repository.common.AbstractRepository;
 
+import java.util.Set;
+
 public interface ExecutorRepository extends AbstractRepository<Executor> {
+    Set<Executor> findAllByIdInAndDeletedIsFalse(Set<Long> ids);
 }
--- service/src/main/java/ru/axbit/service/service/impl/ExecutorServiceImpl.java
+++ service/src/main/java/ru/axbit/service/service/impl/ExecutorServiceImpl.java
@@ -1,16 +1,37 @@
 package ru.axbit.service.service.impl;
 
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.axbit.domain.domain.user.Executor;
+import ru.axbit.domain.repository.ExecutorRepository;
 import ru.axbit.service.service.ExecutorService;
+import ru.axbit.service.service.soap.mapper.response.ResponseMapper;
+import ru.axbit.vborovik.competence.filtertypes.GetExecutorListFilterType;
 import ru.axbit.vborovik.competence.userservice.types.GetExecutorListRequest;
 import ru.axbit.vborovik.competence.userservice.types.GetExecutorListResponse;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 @Service
+@Transactional
+@AllArgsConstructor
 public class ExecutorServiceImpl implements ExecutorService {
+    private final ExecutorRepository executorRepository;
+
     @Override
     public GetExecutorListResponse getExecutorList(GetExecutorListRequest body) {
-        var response = new GetExecutorListResponse();
+        var executors = getExecutor(body.getFilter());
 
-        return response;
+        return ResponseMapper.mapGetExecutorResponse(executors);
+    }
+
+    private Set<Executor> getExecutor(GetExecutorListFilterType filter) {
+        if (Objects.isNull(filter)) return new HashSet<>();
+        Set<Long> executorIds = new HashSet<>(filter.getExecutorId());
+
+        return executorRepository.findAllByIdInAndDeletedIsFalse(executorIds);
     }
 }
