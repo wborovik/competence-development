96000be2c7c27c2038a5919d68282d693697c657
vborovik <w.borovik@mail.ru>
2023-08-18 08:03:44 +0300

    COMPETENCE-13 - В wsdl и xsd добавлен метод getOrderLiist и его вх/вых типы данных.

--- service/src/main/java/ru/axbit/service/service/OrderService.java
+++ service/src/main/java/ru/axbit/service/service/OrderService.java
@@ -1,4 +1,8 @@
 package ru.axbit.service.service;
 
+import ru.axbit.vborovik.competence.userservice.types.v1.GetOrderListRequest;
+import ru.axbit.vborovik.competence.userservice.types.v1.GetOrderListResponse;
+
 public interface OrderService {
+    GetOrderListResponse getOrderList(GetOrderListRequest body);
 }
--- service/src/main/java/ru/axbit/service/service/impl/OrderServiceImpl.java
+++ service/src/main/java/ru/axbit/service/service/impl/OrderServiceImpl.java
@@ -1,8 +1,20 @@
 package ru.axbit.service.service.impl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.axbit.domain.repository.OrderRepository;
 import ru.axbit.service.service.OrderService;
+import ru.axbit.vborovik.competence.userservice.types.v1.GetOrderListRequest;
+import ru.axbit.vborovik.competence.userservice.types.v1.GetOrderListResponse;
 
 @Service
+@AllArgsConstructor
 public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+
+
+    @Override
+    public GetOrderListResponse getOrderList(GetOrderListRequest body) {
+        return null;
+    }
 }
