de8e72ea444eac0ca6e0b00f6ecf33b03c27abfc
vborovik <w.borovik@mail.ru>
2023-08-22 08:44:45 +0300

    COMPETENCE-14 - добавлено поле check в Order.

--- domain/src/main/java/ru/axbit/domain/domain/order/Order.java
+++ domain/src/main/java/ru/axbit/domain/domain/order/Order.java
@@ -1,14 +1,18 @@
 package ru.axbit.domain.domain.order;
 
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.FieldNameConstants;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 import ru.axbit.domain.domain.cls.ClsOrderCategory;
 import ru.axbit.domain.domain.common.AuditEntity;
 import ru.axbit.domain.domain.user.Customer;
 import ru.axbit.domain.domain.user.Executor;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -20,9 +24,13 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "orders")
 @FieldNameConstants
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 public class Order extends AuditEntity {
 
     private String title;
+
+    @Type(type = "jsonb")
+    private JsonNode check;
 
     @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
