6b56ad699d7cfb2dd3b4e211796b59c0ff8e2c90
Vladimir Borovik <w.borovik@mail.ru>
2023-09-07 03:51:05 +0000

    Feature/competence 37

--- service/src/main/java/ru/axbit/service/service/business/robot/ContactKafkaService.java
+++ service/src/main/java/ru/axbit/service/service/business/robot/ContactKafkaService.java
@@ -1,4 +1,7 @@
 package ru.axbit.service.service.business.robot;
 
+/**
+ *
+ */
 public interface ContactKafkaService {
 }
--- service/src/main/java/ru/axbit/service/service/business/robot/impl/ContactKafkaServiceImpl.java
+++ service/src/main/java/ru/axbit/service/service/business/robot/impl/ContactKafkaServiceImpl.java
@@ -2,6 +2,9 @@ package ru.axbit.service.service.business.robot.impl;
 
 import ru.axbit.service.service.business.robot.ContactKafkaService;
 
+/**
+ *
+ */
 public class ContactKafkaServiceImpl implements ContactKafkaService {
 
 }
